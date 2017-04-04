5cfdcab90050bb9a7c1615f691335ddb7e99f618
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2017-04-04 22:28:04 +0300

    second hw finish

--- assignment-2/src/PartA/main.ts
+++ assignment-2/src/PartA/main.ts
@@ -5,53 +5,115 @@ d3.json('exchange_rates.json', (rawData) => {
   for (let i of rawData) data[i['base']] = i['rates'];
   let rates: { [key: string]: { [key: string]: number } } = data;
   let countries: Array<string> = d3.keys(data);
-  let height = 500, width = 1550, bottumPadding = 40, topPadding = 20;
+  let height = 500, width = 1550, bottumPadding = 40, topPadding = 20, leftPadding = 60;
 
-  d3.select('select').selectAll('option')
-    .data(countries)
-    .enter()
-    .append('option')
-    .attr('value', (d) => d)
-    .text((d) => d);
-
-  let currency = 'USD';
-
-  let xsca = d3.scaleLinear()
-    .domain([0, 20])
+  let linearScaler = d3.scaleLinear()
+    .domain([20, 0])
     .range([0, height]);
 
+  let logScaler = d3.scaleLog()
+    .domain([20, 1])
+    .range([0, height]);
+
+  let scaler = linearScaler;
   function xscaler(d) {
-    return xsca(Math.min(20, d));
+    let scalerName = d3.select('input[name=scaler]:checked').attr('value');
+    if (scalerName == 'linear') {
+      scaler = linearScaler;
+      return scaler(Math.min(20, d));
+    }
+    else if (scalerName == 'log') {
+      scaler = logScaler;
+      return scaler(Math.min(20, d + 1));
+    }
   }
 
   let svg = d3.select('svg')
-    .attr('width', width)
+    .attr('width', width + leftPadding)
     .attr('height', topPadding + height + bottumPadding);
 
+  svg.append('line')
+    .attr('x1', leftPadding - 10)
+    .attr('x2', width + leftPadding)
+    .attr('y1', height + topPadding + 1)
+    .attr('y2', height + topPadding + 1);
+
   let rectWidth = 50;
+  let currency = 'USD';
 
   let rects = svg.selectAll('rect')
     .data(d3.values(rates[currency]))
     .enter();
 
   rects.append('rect')
-    .attr('x', (d, i) => i * rectWidth)
-    .attr('y', (d) => height + topPadding - xscaler(d))
-    .attr('height', (d) => xscaler(d))
-    .attr('width', rectWidth / 2);
+    .attr('x', (d, i) => i * rectWidth + leftPadding)
+    .attr('y', (d) => xscaler(d) + topPadding)
+    .attr('width', rectWidth / 2)
+    .attr('height', (d) => height - xscaler(d));
 
   rects.append('text')
-    .attr('x', (d, i) => i * rectWidth)
-    .attr('y', (d) => height + topPadding - xscaler(d) - 5)
-    .text((d) => d > 20 ? d : "");
+    .attr('x', (d, i) => i * rectWidth + leftPadding - 5)
+    .attr('y', (d) => 15)
+    .attr('class', 'rects-texts')
+    .text((d) => d)
+    .style('fill', 'white');
 
   svg.append('g').selectAll('text')
     .data(d3.keys(rates[currency]))
     .enter()
     .append('text')
-    .attr('x', (d, i) => i * rectWidth)
+    .attr('class', 'rects-labels')
+    .attr('x', (d, i) => i * rectWidth + leftPadding)
     .attr('y', (d) => height + topPadding + bottumPadding / 2)
     .text(d => d);
 
+  function render() {
+
+    let rects = svg.selectAll('rect')
+      .data(d3.values(rates[currency]));
+
+    rects.transition().duration(250)
+      .attr('y', (d) => xscaler(d) + topPadding)
+      .attr('height', (d) => height - xscaler(d));
+
+    svg.selectAll('.rects-texts')
+      .data(d3.values(rates[currency]))
+      .transition()
+      .delay(250)
+      .text((d) => d)
+      .style('fill', (d) => d >= 20 ? 'black' : 'white');
+
+    svg.selectAll('text.rects-labels')
+      .data(d3.keys(rates[currency]))
+      .text(d => d);
+
+    svg.selectAll('g#axis').remove();
+
+    var xAxis = d3.axisLeft(scaler).ticks(5);
+    svg.append("g")
+      .attr('id', 'axis')
+      .attr("transform", "translate(" + (leftPadding - 5) + "," + topPadding + ")")
+      .call(xAxis);
+  }
+
+  let select = d3.select('select');
+
+  select.selectAll('option')
+    .data(countries)
+    .enter()
+    .append('option')
+    .attr('id', (d) => d)
+    .attr('value', (d) => d)
+    .text((d) => d);
+
+  select.on('change', function() {
+    currency = d3.select(this).property('value');
+    render();
+  });
+
+  d3.select('#USD').attr("selected", '');
+  render();
+
+  d3.selectAll('input[name=scaler]').on('change', render);
 
 });
--- assignment-2/src/PartB/main.ts
+++ assignment-2/src/PartB/main.ts
@@ -1,3 +1,133 @@
-/// <reference path="../node_modules/@types/d3/index.d.ts"/>
+/// <reference path="../../node_modules/@types/d3/index.d.ts"/>
 
-d3.select("body").append("p").text("asdasd");
+interface Country {
+  name: string;
+  gdp: number;
+  population: number;
+}
+
+
+class CountriesScatter {
+
+  padding = 70;
+  height = 500;
+  width = 500;
+  leftPadding = 75;
+
+  populationScaler;
+  gdpScaler;
+  colorScaler;
+
+  constructor() {
+  }
+
+  viewInit() {
+    this.loadData((data) => {
+      this.initScalers(data);
+      this.render(data);
+    });
+  }
+
+  render(data: Country[]) {
+    let svgScatter = d3.select('#scatter').append('svg')
+      .attr('height', this.height + 2 * this.padding)
+      .attr('width', this.width + 2 * this.padding + this.leftPadding);
+
+    svgScatter.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', (d) => this.gdpScaler(d.gdp) + this.padding + this.leftPadding)
+      .attr('cy', (d) => this.populationScaler(d.population) + this.padding)
+      .transition()
+      .duration(1500)
+      .attr('r', (d) => 10)
+      .attr('fill', (d) => this.colorScaler(d.name))
+
+    svgScatter.append('text')
+      .attr('x', (this.width + 2 * this.padding + this.leftPadding) / 2)
+      .attr('y', this.height + 2 * this.padding)
+      .text('GDP GROWTH (%)')
+
+    svgScatter.append('text')
+      .attr('x', 0)
+      .attr('y', (this.height + 2 * this.padding) / 2)
+      .attr('transform', `rotate(-90, 0, ${(this.height + 2 * this.padding) / 2}) translate(-100, 40)`)
+      .text('POPULATION GROWTH (%)')
+
+    let svgLabels = d3.select('#labels').append('svg')
+      .attr('height', 800)
+      .attr('width', 200);
+
+    svgLabels.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', 10)
+      .attr('cy', (d, i) => i * 30 + 10)
+      .attr('r', 10)
+      .attr('fill', (d) => this.colorScaler(d.name));
+
+
+    svgLabels.selectAll('text')
+      .data(data)
+      .enter()
+      .append('text')
+      .attr('x', 30)
+      .attr('y', (d, i) => i * 30 + 15)
+      .text((d) => d.name);
+
+    let populationAxis = d3.axisLeft(this.populationScaler).ticks(10);
+
+    svgScatter.append("g")
+      .attr('id', 'yaxis')
+      .attr("transform", `translate(${this.leftPadding + this.padding - 15},${this.padding})`)
+      .call(populationAxis);
+
+    let gdpAxis = d3.axisBottom(this.gdpScaler).ticks(5);
+
+    svgScatter.append("g")
+      .attr('id', 'xaxis')
+      .attr("transform", `translate(${this.leftPadding + this.padding},${this.height + this.padding + 15})`)
+      .call(gdpAxis);
+
+  }
+
+  initScalers(data: Country[]) {
+    this.populationScaler = d3.scaleLinear()
+      .domain([d3.min(data, (d) => d.population), d3.max(data, (d) => d.population)])
+      .range([this.height, 0]);
+
+    this.gdpScaler = d3.scaleLinear()
+      .domain([d3.min(data, (d) => d.gdp), d3.max(data, (d) => d.gdp)])
+      .range([0, this.width]);
+
+    this.colorScaler = d3.scaleOrdinal()
+      .domain(data.map(d => d.name))
+      .range([...d3.schemeCategory20, ...d3.schemeCategory20b]);
+
+
+  }
+
+  loadData(callback: (data: Country[]) => void) {
+    d3.json('countries.json', (rawData) => {
+      let data = rawData
+        .filter(d => d.continent == 'Europe')
+        .map(d => {
+          let year2000 = d.years.filter(y => y.year == 2000)[0];
+          let year2012 = d.years.filter(y => y.year == 2012)[0];
+
+          return {
+            'name': d.name,
+            'gdp': year2012.gdp - year2000.gdp,
+            'population': year2012.population - year2000.population
+          }
+        });
+      callback(data);
+    });
+  }
+
+}
+
+let scatter = new CountriesScatter();
+scatter.viewInit();
