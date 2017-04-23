9c5ad5f83de0ccc5ab6da84686ec1705d896cea7
cs <hasancelik@std.sehir.edu.tr>
2017-04-23 17:08:20 +0300

    update in code

--- assignment-3/src/main.ts
+++ assignment-3/src/main.ts
@@ -1,56 +1,69 @@
 /// <reference path="../node_modules/@types/d3/index.d.ts"/>
 
+class MainComponent {
+
+    viewInit() {
+        this.loadData((data) => {
+            new CountriesPie('svg', data).viewInit();
+        });
+    }
+
+    loadData(callback: (data) => void) {
+        d3.json('countries.json', (rawData) => {
+            let data = {};
+
+            rawData.forEach(d => {
+                if (!(d.continent in data)) data[d.continent] = [];
+                data[d.continent].push([d.name, d.years.filter(y => y.year == 2012)[0].gdp]);
+            });
+
+            for (let continent in data)
+                data[continent] = data[continent].sort((x, y) => y[1] - x[1]).slice(0, 10);
+
+            callback(data);
+        });
+    }
+}
+
+
 class CountriesPie {
 
-  padding = 50;
-  height = 500;
-  width = 500;
-  data;
-  pie = d3.pie();
-  outherRadius = 100;
-  arc = d3.arc().innerRadius(0).outerRadius(this.outherRadius);
-  svg = d3.select('svg').attr('height', this.height).attr('width', this.width);
-  color = d3.scaleOrdinal(d3.schemeCategory10);
+    data;
+    svg;
+    padding = 50;
+    height = 500;
+    width = 500;
+    pie = d3.pie();
+    outherRadius = 100;
+    arc = d3.arc().innerRadius(0).outerRadius(this.outherRadius);
+    color = d3.scaleOrdinal(d3.schemeCategory10);
 
-  populationScaler;
-  gdpScaler;
-  colorScaler;
+    populationScaler;
+    gdpScaler;
+    colorScaler;
 
-  viewInit() {
-    this.loadData((data) => {
-      let europe = data['Europe'];
-      this.svg.append("g")
-        .attr("transform", "translate(" + this.outherRadius + ", " + this.outherRadius + ")")
-        .selectAll("path")
-        .data(this.pie(europe.map(x => x[1])))
-        .enter()
-        .append("path")
-        .attr("d", (d: any) => this.arc(d))
-        .attr("fill", (d, i: any) => this.color(i))
-      console.log(europe);
-    });
-  }
+    constructor(selector, data) {
+        this.data = data;
+        this.svg = d3.select(selector);
+    }
 
-  render() { }
+    viewInit() {
+        let europe = this.data['Europe'];
+        this.svg.attr('height', this.height).attr('width', this.width);
+        this.svg.append("g")
+            .attr("transform", "translate(" + this.outherRadius + ", " + this.outherRadius + ")")
+            .selectAll("path")
+            .data(this.pie(europe.map(x => x[1])))
+            .enter()
+            .append("path")
+            .attr("d", (d: any) => this.arc(d))
+            .attr("fill", (d, i: any) => this.color(i))
+        console.log(europe);
+    }
 
-  loadData(callback: (data) => void) {
-    d3.json('countries.json', (rawData) => {
-      let data = {};
-
-      rawData.forEach(d => {
-        if (!(d.continent in data)) data[d.continent] = [];
-        data[d.continent].push([d.name, d.years.filter(y => y.year == 2012)[0].gdp]);
-      });
-
-      for (let continent in data)
-        data[continent] = data[continent].sort((x, y) => y[1] - x[1]).slice(0, 10);
-
-      this.data = data;
-      callback(data);
-    });
-  }
+    render() { }
 
 }
 
-let pie = new CountriesPie();
-pie.viewInit();
+let main = new MainComponent();
+main.viewInit();
